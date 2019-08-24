390b2533461fd8e981b3d266e8fdc48135e06e01
caio neres <caioneres40@gmail.com>
2019-08-24 19:30:32 -0300

    0 vai

---
+++ app/src/main/java/com/tcc/serviceapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tcc.serviceapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tcc.serviceapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

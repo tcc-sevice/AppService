4386dca415de76ceddb16344f2ef27dddd192b72
Leonardo Durães <leonardolp6412@gmail.com>
2019-09-04 20:44:06 -0300

    Atualizações (dependencias, gradle a androidX)
    Atualizado para as ultimas versões possíveis

--- app/src/main/java/com/tcc/serviceapp/activity/EnderecoActivity.java
+++ app/src/main/java/com/tcc/serviceapp/activity/EnderecoActivity.java
@@ -2,8 +2,8 @@ package com.tcc.serviceapp.activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,17 +12,8 @@ import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.tcc.serviceapp.R;
-import com.tcc.serviceapp.helper.ConfiguracaoFirebase;
 import com.tcc.serviceapp.model.Endereco;
-import com.tcc.serviceapp.model.Usuario;
 
 public class EnderecoActivity extends AppCompatActivity {
 
